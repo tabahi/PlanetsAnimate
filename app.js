7f3b4d7accd72c31b7d7951f1021f2f013478f52
Tabahi Repository <arehman@bournemouth.ac.uk>
2022-08-08 10:13:06 +0100

    yo

--- app.js
+++ app.js
@@ -1,11 +1,66 @@
 const { get } = require('http');
-const https = require('https')
+const https = require('https');
+const { exit } = require('process');
 fs = require('fs');
 //references:
 //https://ssd.jpl.nasa.gov/horizons/manual.html
 
 const planet_names = ["sun",  "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus" , "neptune", "pluto", "moon"];
 const planet_codes = {"sun": '10', "moon": '301', "mercury": '199', "venus": '299', "earth": '399', "mars": "499", "jupiter": "599", "saturn": "699", "uranus": "799", "neptune": "899", "pluto": "999"};
+
+
+function download_ephemeris()
+{
+
+    let ts_now = new Date(2019, 0, 01, 2, 0, 0, 0);
+    
+    function chain_download()
+    {
+        console.log(ts_now);
+        let all_await = [];
+        
+        for (let pl=0; pl<planet_names.length; pl++)
+            if(planet_names[pl]!=="earth")
+                all_await.push(planet_find(planet_names[pl], "earth", ts_now));
+        
+        Promise.all(all_await).then(results => {
+            let ts_ms = ts_now.getUTCMilliseconds() + 86400000; //add a day
+            ts_now.setUTCMilliseconds(ts_ms);
+            if(ts_now.getUTCFullYear()!=2025) chain_download();
+        }).catch((error) => {
+            console.error(error);
+            console.log(ts_now);
+        });
+        
+    }
+    chain_download();
+}
+download_ephemeris();
+
+
+
+function planet_find(target_body, center_body, timestamp)
+{
+    return new Promise(function(resolve, reject) {
+    
+    let ts_bounds = get_boundary_timestamps(timestamp);
+
+    //console.log(timestamp, ts_bounds.upper);
+    //console.log(timestamp, ts_bounds.lower);
+    
+    if(ts_bounds.upper.getUTCDate()==ts_bounds.lower.getUTCDate()) ts_bounds.upper.setTime(ts_bounds.upper.getTime() + 86400000);
+    let up_date = String(ts_bounds.upper.getUTCFullYear()) + '-' + String(ts_bounds.upper.getUTCMonth()+1) + '-' + String(ts_bounds.upper.getUTCDate());
+    let lo_date = String(ts_bounds.lower.getUTCFullYear()) + '-' + String(ts_bounds.lower.getUTCMonth()+1) + '-' + String(ts_bounds.lower.getUTCDate());
+    
+
+    ensure_epi_on_date(target_body, center_body, lo_date, up_date, String(ts_bounds.lower.getUTCFullYear())).then(json_data => {
+        
+        //console.log(json_data[String(ts_bounds.lower.getTime()/10000)]);
+        resolve(true);
+    }).catch(e=>{ reject(e); return console.error(e);})
+});
+}
+
 
 function get_ep(target_body_code, center_body_code = '399', start_date='2022-01-01', end_date='2022-01-02', step_mins='10')
 {
@@ -78,25 +133,15 @@ function get_ep(target_body_code, center_body_code = '399', start_date='2022-01-
 }
 
 
-let ts_now = new Date();
 
-for (let pl=0; pl<=10; pl++)
-    if(planet_names[pl]!=="earth")
-    planet_find(planet_names[pl], "earth", ts_now);
-
-    
-
-function planet_find(target_body, center_body, timestamp)
+function get_boundary_timestamps(timestamp)
 {
-    timestamp.setYear(2020);
-    timestamp.setMinutes(56);
     let exact_mins = timestamp.getUTCMinutes();
     let upper_mins = exact_mins + (10 - (exact_mins%10));
     let lower_mins = exact_mins - (exact_mins%10);
 
     let up_prob = (exact_mins - lower_mins)/10;
     let lo_prob = (upper_mins - exact_mins)/10;
-    console.log(lo_prob, up_prob, lo_prob + up_prob);
 
 
     timestamp.setSeconds(0); timestamp.setMilliseconds(0);
@@ -104,21 +149,7 @@ function planet_find(target_body, center_body, timestamp)
     let up_ts = new Date(timestamp);
     lo_ts.setMinutes(lower_mins);
     up_ts.setMinutes(upper_mins);
-
-    //console.log(timestamp, up_ts);
-    //console.log(timestamp, lo_ts);
-    
-    if(up_ts.getUTCDate()==lo_ts.getUTCDate())  up_ts.setTime(up_ts.getTime() + 86400000);
-
-    let up_date = String(up_ts.getUTCFullYear()) + '-' + String(up_ts.getUTCMonth()+1) + '-' + String(up_ts.getUTCDate());
-    let lo_date = String(lo_ts.getUTCFullYear()) + '-' + String(lo_ts.getUTCMonth()+1) + '-' + String(lo_ts.getUTCDate());
-    
-
-    ensure_epi_on_date(target_body, center_body, lo_date, up_date, String(lo_ts.getUTCFullYear())).then(json_data => {
-        
-        //console.log(json_data[String(lo_ts.getTime()/10000)]);
-    }).catch(e=>{console.error(e); return;})
-    
+    return {lower: lo_ts, upper: up_ts, lower_prob: lo_prob, upper_prob: up_prob};
 }
 
 function ensure_epi_on_date(target_body, center_body, lo_date, up_date, year_full)
@@ -256,4 +287,162 @@ function save_json(filename, data)
       console.log('Saved to ' + filename);
     });
 }
+
+
+/* Notes
+
+Column meaning:
+ 
+TIME
+
+  Times PRIOR to 1962 are UT1, a mean-solar time closely related to the
+prior but now-deprecated GMT. Times AFTER 1962 are in UTC, the current
+civil or "wall-clock" time-scale. UTC is kept within 0.9 seconds of UT1
+using integer leap-seconds for 1972 and later years.
+
+  Conversion from the internal Barycentric Dynamical Time (TDB) of solar
+system dynamics to the non-uniform civil UT time-scale requested for output
+has not been determined for UTC times after the next July or January 1st.
+Therefore, the last known leap-second is used as a constant over future
+intervals.
+
+  Time tags refer to the UT time-scale conversion from TDB on Earth
+regardless of observer location within the solar system, although clock
+rates may differ due to the local gravity field and no analog to "UT"
+may be defined for that location.
+
+  Any 'b' symbol in the 1st-column denotes a B.C. date. First-column blank
+(" ") denotes an A.D. date. Calendar dates prior to 1582-Oct-15 are in the
+Julian calendar system. Later calendar dates are in the Gregorian system.
+
+  NOTE: "n.a." in output means quantity "not available" at the print-time.
+ 
+ 'R.A._____(ICRF)_____DEC' =
+  Astrometric right ascension and declination of the target center with
+respect to the observing site (coordinate origin) in the reference frame of
+the planetary ephemeris (ICRF). Compensated for down-leg light-time delay
+aberration.
+
+  Units: RA  in hours-minutes-seconds of time,    HH MM SS.ff{ffff}
+         DEC in degrees-minutes-seconds of arc,  sDD MN SC.f{ffff}
+ 
+ 'APmag   S-brt' =
+  The targets' approximate apparent visual magnitude and surface brightness.
+For planets and natural satellites, output is restricted to solar phase angles
+covered by observational data. Outside the observed phase angle range, "n.a."
+may be output to avoid extrapolation beyond the limit of model validity.
+
+   For Earth-based observers, the estimated dimming due to atmospheric
+absorption (extinction) is available as a separate, requestable quantity.
+
+   Surface brightness is the average airless visual magnitude of a
+square-arcsecond of the illuminated portion of the apparent disk. It is
+computed only if the target radius is known.
+
+   Units: MAGNITUDES & MAGNITUDES PER SQUARE ARCSECOND
+ 
+ 'delta      deldot' =
+   Apparent range ("delta", light-time aberrated) and range-rate ("delta-dot")
+of the target center relative to the observer. A positive "deldot" means the
+target center is moving away from the observer, negative indicates movement
+toward the observer.  Units: AU and KM/S
+ 
+ 'S-O-T /r' =
+   Sun-Observer-Target apparent SOLAR ELONGATION ANGLE seen from the observers'
+location at print-time.
+
+   The '/r' column provides a code indicating the targets' apparent position
+relative to the Sun in the observers' sky, as described below:
+
+   Case A: For an observing location on the surface of a rotating body, that
+body rotational sense is considered:
+
+    /T indicates target TRAILS Sun   (evening sky: rises and sets AFTER Sun)
+    /L indicates target LEADS Sun    (morning sky: rises and sets BEFORE Sun)
+
+   Case B: For an observing point that does not have a rotational model (such
+as a spacecraft), the "leading" and "trailing" condition is defined by the
+observers' heliocentric ORBITAL motion:
+
+    * If continuing in the observers' current direction of heliocentric
+       motion would encounter the targets' apparent longitude first, followed
+       by the Sun's, the target LEADS the Sun as seen by the observer.
+
+    * If the Sun's apparent longitude would be encountered first, followed
+       by the targets', the target TRAILS the Sun.
+
+   Two other codes can be output:
+    /* indicates observer is Sun-centered    (undefined)
+    /? Target is aligned with Sun center     (no lead or trail)
+
+   The S-O-T solar elongation angle is numerically the minimum separation
+angle of the Sun and target in the sky in any direction. It does NOT indicate
+the amount of separation in the leading or trailing directions, which would
+be defined along the equator of a spherical coordinate system.
+
+   Units: DEGREES
+ 
+ 'S-T-O' =
+   The Sun-Target-Observer angle; the interior vertex angle at target center
+formed by a vector from the target to the apparent center of the Sun (at
+reflection time on the target) and the apparent vector from target to the
+observer at print-time. Slightly different from true PHASE ANGLE (requestable
+separately) at the few arcsecond level in that it includes stellar aberration
+on the down-leg from target to observer.  Units: DEGREES
+ 
+ 'Sky_motion  Sky_mot_PA  RelVel-ANG' =
+  Total apparent angular rate of the target in the plane-of-sky. "Sky_mot_PA"
+is the position angle of the target's direction of motion in the plane-of-sky,
+measured counter-clockwise from the apparent of-date north pole direction.
+"RelVel-ANG" is the flight path angle of the target's relative motion with
+respect to the observer's line-of-sight, in the range [-90,+90], where positive
+values indicate motion away from the observer, negative values are toward the
+observer:
+
+  -90 = target is moving directly toward the observer
+    0 = target is moving at right angles to the observer's line-of-sight
+  +90 = target is moving directly away from the observer
+
+UNITS:  ARCSECONDS/MINUTE, DEGREES, DEGREES
+ 
+ 'Lun_Sky_Brt  sky_SNR' =
+  Sky brightness due to moonlight scattered by Earth's atmosphere at the
+target's position in the sky. "sky_SNR" is the visual signal-to-noise ratio
+(SNR) of the target's surface brightness relative to background sky. Output
+only for topocentric Earth observers when both the Moon and target are above
+the local horizon and the Sun is in astronomical twilight (or further) below
+the horizon, and the target is not the Moon or Sun. If all conditions are
+not met, "n.a." is output. Galactic brightness, local sky light-pollution
+and weather are NOT considered. Lunar opposition surge is considered. The
+value returned is accurate under ideal conditions at the approximately 8-23%
+level, so is a useful but not definitive value.
+
+  If the target-body radius is also known, "sky_SNR" is output. This is the
+approximate visual signal-to-noise ratio of the target's brightness divided
+by lunar sky brightness. When sky_SNR < 1, the target is dimmer than the
+ideal moonlight-scattering background sky, so unlikely to be detectable at
+visual wavelengths. In practice, visibility requires sky_SNR > 1 and a
+detector sensitive enough to reach the target's magnitude, even if it isn't
+washed out by moonlight. When relating magnitudes and brightness values,
+keep in mind their logarithmic relationship m2-m1 = -2.5*log_10(b2/b1).
+
+  UNITS: VISUAL MAGNITUDES / ARCSECOND^2, and unitless ratio
+
+Computations by ...
+
+    Solar System Dynamics Group, Horizons On-Line Ephemeris System
+    4800 Oak Grove Drive, Jet Propulsion Laboratory
+    Pasadena, CA  91109   USA
+
+    General site: https://ssd.jpl.nasa.gov/
+    Mailing list: https://ssd.jpl.nasa.gov/email_list.html
+    System news : https://ssd.jpl.nasa.gov/horizons/news.html
+    User Guide  : https://ssd.jpl.nasa.gov/horizons/manual.html
+    Connect     : browser        https://ssd.jpl.nasa.gov/horizons/app.html#/x
+                  API            https://ssd-api.jpl.nasa.gov/doc/horizons.html
+                  command-line   telnet ssd.jpl.nasa.gov 6775
+                  e-mail/batch   https://ssd.jpl.nasa.gov/ftp/ssd/hrzn_batch.txt
+                  scripts        https://ssd.jpl.nasa.gov/ftp/ss
+
+*/
 
--- index.js
+++ index.js
@@ -4,15 +4,29 @@ var ctx = canvasEle.getContext("2d");
 var center_x = 0;
 var center_y = 0;
 var max_radius = 0;
+const max_AU = Math.sqrt(40);
 
-var ts = new Date(1991, 11, 08, 0, 0, 0, 0);
+var ts = new Date(2019, 0, 01, 2, 0, 0, 0);
 
+var bodies = [
+  {name: "sun",     color: "rgb(255,213,0)",      size: 5, au_factor: 1}, //0
+  {name: "mercury", color: "rgb(153, 204, 255)",  size: 2,  au_factor: 1},
+  {name: "venus",   color: "rgb(255, 102, 255)",  size: 3,  au_factor: 1},
+  {name: "earth",   color: "rgb(0,150,50)",       size: 3,  au_factor: 1}, //3
+  {name: "mars",    color: "rgb(255, 51, 0)",     size: 3,  au_factor: 1},
+  {name: "jupiter", color: "rgb(204, 153, 0)",    size: 4,  au_factor: 1},
+  {name: "saturn",  color: "rgb(153, 153, 102)",  size: 4,  au_factor: 1},
+  {name: "uranus",  color: "rgb(0, 204, 255)",    size: 4,  au_factor: 1},
+  {name: "neptune", color: "rgb(51, 51, 255)",    size: 4,  au_factor: 1},
+  {name: "pluto",   color: "rgb(153, 0, 0)",      size: 2,  au_factor: 1}, //9
+  {name: "moon",    color: "rgb(255,255,255)",    size: 1,  au_factor: 6}, //10
+];
 
 function init()
 {
   reset_canvas();
   clearInterval(interval_id);
-  interval_id = setInterval(animate_objects, 1000);
+  interval_id = setInterval(animate_objects, 100);
 }
 
 
@@ -40,25 +54,13 @@ function reset_canvas()
   ctx.stroke();
   ctx.fill();
   //Earth for Geocentric
-  //add_object(center_x, center_y, "rgb(0,150,50)", 0, 0, 10);
+  add_object(center_x, center_y, bodies[3]["color"], 0, 0, bodies[3]["size"]);
 
   //Sun for Heliocentric
-  add_object(center_x, center_y, "rgb(255,213,0)", 0, 0, 20);
+  //add_object(center_x, center_y, bodies[0]["color"], 0, 0, 20);
 }
 
-var bodies = [
-  {name: "sun",     color: "rgb(255,213,0)",      size: 20, au_factor: 1}, //0
-  {name: "mercury", color: "rgb(153, 204, 255)",  size: 3,  au_factor: 1},
-  {name: "venus",   color: "rgb(255, 102, 255)",  size: 5,  au_factor: 1},
-  {name: "em_barry",color: "rgb(0,150,50)",       size: 5,  au_factor: 1}, //3
-  {name: "mars",    color: "rgb(255, 51, 0)",     size: 4,  au_factor: 1},
-  {name: "jupiter", color: "rgb(204, 153, 0)",    size: 7,  au_factor: 1},
-  {name: "saturn",  color: "rgb(153, 153, 102)",  size: 7,  au_factor: 1},
-  {name: "uranus",  color: "rgb(0, 204, 255)",    size: 6,  au_factor: 1},
-  {name: "neptune", color: "rgb(51, 51, 255)",    size: 6,  au_factor: 1},
-  {name: "pluto",   color: "rgb(153, 0, 0)",      size: 4,  au_factor: 1}, //9
-  {name: "moon",    color: "rgb(255,255,255)",    size: 4,  au_factor: 1}, //10
-];
+
 
 
 
@@ -66,13 +68,15 @@ function animate_objects()
 {
   let ts_ms = ts.getUTCMilliseconds() + 86400000;
   ts.setUTCMilliseconds(ts_ms);
-  //console.log(ts);
- 
-  place_kepler_objects(ts);
+  console.log(ts);
+  //reset_canvas();
+  //place_kepler_objects(ts);
+  place_horizon_objects(ts);
 }
 
 function add_object(center_x, center_y, color="rgb(100,100,100)", angle=0, dist=20, size=10)
 {
+  //angle in degrees, dist = 0 to max_radius in px. size in px
   let radians = ((angle+180)/360)*-2*Math.PI;
   let x = dist * Math.cos(radians);
   let y = dist * Math.sin(radians);
@@ -81,14 +85,86 @@ function add_object(center_x, center_y, color="rgb(100,100,100)", angle=0, dist=
   ctx.fillStyle = color;
   ctx.beginPath();
   ctx.arc(center_x + x, center_y + y, size, 0, 2 * Math.PI);
+  ctx.strokeStyle = "#353535";
   ctx.stroke();
   ctx.fill();
 }
 
+function place_horizon_objects(timestamp)
+{
+  for (let p=0; p<bodies.length; p++)
+  {
+    if(bodies[p]["name"]!="earth")
+    get_horizon_epi(bodies[p]["name"], "earth", timestamp).then(epi_info => {
+
+      add_object(center_x, center_y, bodies[p]["color"], epi_info["RA"], max_radius * (Math.sqrt(epi_info["au"])/max_AU) * bodies[p]["au_factor"], bodies[p]["size"]);
+    }).catch(e=>{ console.error(e); console.log("ERROR: Ephemeris unavailable for this time for " + bodies[p]["name"]); clearInterval(interval_id); return;});
+
+  }
+}
+
+get_horizon_epi("mercury", "earth", ts);
+
+function get_horizon_epi(target_body, center_body="earth", timestamp=new Date())
+{
+
+  return new Promise(function(resolve, reject) {
+  let ts_bounds = get_boundary_timestamps(timestamp);
+  
+  let ts_label = String(ts_bounds.lower.getTime()/10000);
+  if(ts_bounds.upper.getUTCDate()==ts_bounds.lower.getUTCDate()) ts_bounds.upper.setTime(ts_bounds.upper.getTime() + 86400000);
+  let lo_date = String(ts_bounds.lower.getUTCFullYear()) + '-' + String(ts_bounds.lower.getUTCMonth()+1) + '-' + String(ts_bounds.lower.getUTCDate());
+  let up_date = String(ts_bounds.upper.getUTCFullYear()) + '-' + String(ts_bounds.upper.getUTCMonth()+1) + '-' + String(ts_bounds.upper.getUTCDate());
+
+  //console.log(ts_bounds.lower, lo_date);
+  //console.log(ts_bounds.upper, up_date);
+  let epi_file_path = 'epi/' + String(ts_bounds.lower.getUTCFullYear()) + '/' + lo_date + '_' + up_date + '/' + target_body + '_' + center_body + '.json';
+
+  fetch(epi_file_path)
+  .then(response => response.json())
+  .then(json_obj => {
+
+    if(json_obj[ts_label]!==undefined)
+    {
+      //console.log(json_obj[ts_label]);
+      let ephemeris_info = {};
+      ephemeris_info["RA"] = concert_arcs_to_decimal(json_obj[ts_label][0], json_obj[ts_label][1], json_obj[ts_label][2]);
+      ephemeris_info["au"] = json_obj[ts_label][8];
+      ephemeris_info["au_dot"] = json_obj[ts_label][9];
+      resolve(ephemeris_info);
+    }
+    else reject(console.error(ts_label + ' ts_label not found in JSON file ' + epi_file_path));
+  
+  }).catch(err => {reject(console.error(err));});
+});
+}
+
+function get_boundary_timestamps(timestamp)
+{
+  //decimal boundaries... Because database is with 10 minutes steps
+    let exact_mins = timestamp.getUTCMinutes();
+    let upper_mins = exact_mins + (10 - (exact_mins%10));
+    let lower_mins = exact_mins - (exact_mins%10);
+
+    let up_prob = (exact_mins - lower_mins)/10;
+    let lo_prob = (upper_mins - exact_mins)/10;
+
+
+    timestamp.setSeconds(0); timestamp.setMilliseconds(0);
+    let lo_ts = new Date(timestamp);
+    let up_ts = new Date(timestamp);
+    lo_ts.setMinutes(lower_mins);
+    up_ts.setMinutes(upper_mins);
+    return {lower: lo_ts, upper: up_ts, lower_prob: lo_prob, upper_prob: up_prob};
+}
+
+function concert_arcs_to_decimal(arc_deg, arc_min=0, arc_sec=0)
+{
+  return ((arc_deg*15) + (arc_min*0.25) + (arc_sec/240));
+}
 
 function place_kepler_objects(timestamp)
 {
-  let max_AU = Math.sqrt(40);
   
   for (let p=1; p<=8; p++)
   {
